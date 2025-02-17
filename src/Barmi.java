628bf5989f1a385a5ffa0269c6515ae066ebeced
upwindcore <tibor.mercs@protonmail.com>
2025-02-17 10:01:34 +0100

    2. óra - Attila

---
+++ src/Barmi.java
@@ -0,0 +1,10 @@
+public class Barmi {
+
+    public static void main(String[] args) {
+        Haromszog haromszog = new Haromszog();
+        haromszog.a = 3;
+        haromszog.b = 4;
+        haromszog.c = 5;
+        System.out.println(haromszog.kerulet());
+    }
+}
---
+++ src/Haromszog.java
@@ -0,0 +1,20 @@
+public class Haromszog {
+
+    double a;
+
+    double b;
+
+    double c;
+
+    public double kerulet() {
+        return a + b + c;
+    }
+
+    double terulet() {
+        return 0.0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("3szög");
+    }
+}
