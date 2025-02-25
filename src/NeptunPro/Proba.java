3e0dc59f5a0a2608272efd37aafced87476f503c
upwindcore <tibor.mercs@protonmail.com>
2025-02-25 19:38:50 +0100

    4. óra - Iszaly

---
+++ src/NeptunPro/Proba.java
@@ -0,0 +1,12 @@
+package NeptunPro;
+
+public class Proba {
+    public static void main(String[] args) {
+        /*
+        Ember eva = new Ember("Béla", 1988);
+        eva.nev="Éva";
+        //eva.szev=1978;
+        System.out.println(eva.toString());
+         */
+    }
+}
